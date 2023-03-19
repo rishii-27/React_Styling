265ee96395667471f6f70fe5f0484ad48cf7f29f
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-03-19 16:32:10 +0530

    Clearing input fields on Submit

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,34 +5,33 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
-    if (event.target.value.trim().length > 0) {
-      setIsValid(true)
-    }
-    setEnteredValue(event.target.value);
+    const value = event.target.value.trim();
+    setEnteredValue(value);
+    setIsValid(value.length > 0);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
-      setIsValid(false)
+      setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
-        <label>
-          Course Goal
-        </label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} value={enteredValue} />
       </div>
-      <Button type="submit" className={`${!isValid ? 'invalid' : ''}`}>Add Goal</Button>
+      <Button type="submit">
+        Add Goal
+      </Button>
     </form>
   );
 };
